2b477e54b38a2be7c521b19b3e581572ceaf08fa
ReynaldoAVZ <reynaldoavz@outlook.com>
2023-09-19 17:55:47 -0600

    assign03 SimplePriorityQueue Final Version

--- src/assign03/SimplePriorityQueueTest.java
+++ src/assign03/SimplePriorityQueueTest.java
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import java.util.*;
 import static org.junit.jupiter.api.Assertions.*;
-
 // Create the comparators for ints, doubles, and strings
 /**
  * Comparator that defines an ordering among a SimplePriorityQueue of Integers using their value.
